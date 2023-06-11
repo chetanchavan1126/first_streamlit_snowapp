2c249741519d83f23893e344b7847d8eaa32c884
chetanchavan1126 <123924583+chetanchavan1126@users.noreply.github.com>
2023-06-11 10:13:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,20 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#Create repeatable code block (called a function)
+def get_fruityvice_date(this_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+        
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
         streamlit.error("Please select fruit to get information")
     else:
-        #streamlit.write('The user entered ', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-        #streamlit.text(fruityvice_response.json())
-        # write your own comment -what does the next line do? 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        # write your own comment - what does this do?
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
        streamlit.error()
     
